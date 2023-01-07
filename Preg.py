cfa1f0a7fcf1becf09872a8013a6774e2cd0bc4f
luismartelb <martelbonillaluis@gmail.com>
2023-01-07 13:56:21 -0500

    Examen_I

---
+++ Preg.py
@@ -0,0 +1,4 @@
+n = int(input("Ingrese la cantidad de veces que desea repetir la palabra CUENTO: "))
+for i in range(1,n+1,1):
+    print("CUENTO ",i)
+    
---
+++ preg_1.js
@@ -0,0 +1,4 @@
+var n = prompt("Ingrese la cantidad de veces que desea repetir la palabra CUENTO: ")
+for(var i=1;i<=n;i++){
+    document.write("CUENTO ",i+"<br>")
+}
